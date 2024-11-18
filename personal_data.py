b8f5bdc61d6dfed3ed57a214940db43d20ec7b69
Alan A. <58347931+AlanAyy@users.noreply.github.com>
2024-11-18 05:47:53 -0700

    Upload Code

---
+++ personal_data.py
@@ -0,0 +1,12 @@
+PERSON_INFO = {
+    'Alan': {
+        'age': 23,
+        'likes': ['sushi', 'stress', 'meeting new people'],
+        'talk': ['AI', 'cool stories', 'DJing']
+    },
+    'Chris': {
+        'age': 23,
+        'likes': ['dragons', 'HCI', 'games'],
+        'talk': ['skibidi sigma']
+    }
+}
